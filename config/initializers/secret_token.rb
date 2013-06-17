fecdf2ecd222a14a67922ba57b976d29dbdb73bc
pie <pie@pchome.com.tw>
2013-06-17 23:15:54 +0800

    create basic structure for poleStar
    had not made any chage.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoleStar::Application.config.secret_token = '5d964feb910bc78768f515ec50a17bef70c62f09f7fd370982188acc34fe5dfe01f4257a7cf7f2d36581107d4b75357b9734bfd59587444dd5aba4919bf8d32c'
